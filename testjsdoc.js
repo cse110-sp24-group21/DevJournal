bc7f7cf54a5947d14b5c2bb9e1f736d411e6a571
evawlve <130005497+evawlve@users.noreply.github.com>
2024-05-12 15:14:43 -0700

    added jsdoc configuration

---
+++ testjsdoc.js
@@ -0,0 +1,9 @@
+/**
+ * Calculates the sum of two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @return {number} The sum of the two numbers.
+ */
+function add(a, b) {
+    return a + b;
+}
