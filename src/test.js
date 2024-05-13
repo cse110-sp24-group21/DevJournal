502c9c38f6edf4acbc17ef3df2c0630824ccd300
Li-Kane <122249106+Li-Kane@users.noreply.github.com>
2024-05-13 12:48:01 -0700

    Create test.js
    Testing to see if PR functionality in code-climate works

---
+++ src/test.js
@@ -0,0 +1,6 @@
+function multiplyTwo_IntsBadly(intNumber_One, Int_num2){
+    for (var iterataion = 0; iterataion < intNumber_One; iterataion++){
+        Int_num2 += Int_num2
+    }
+    return Int_num2
+}
